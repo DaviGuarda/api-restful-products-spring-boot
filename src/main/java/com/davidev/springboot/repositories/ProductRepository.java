b2d1e8f976d8e1d7a329da743fc0c7b09477f001
Davi <davi.guarda.b@hotmail.com>
2023-07-28 18:03:24 -0300

    first commit

---
+++ src/main/java/com/davidev/springboot/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.davidev.springboot.repositories;
+
+import com.davidev.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
